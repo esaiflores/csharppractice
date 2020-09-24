807e8f79d0cad45dc42ae0c77f5160a75010e949
Esai Flores <flores.esaijonathan@gmail.com>
2020-09-23 21:15:52 -0500

    working on csharp practice

--- Test/adding.cs
+++ Test/adding.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+
 namespace Test
+
 {
-    public class adding
+    class AddAndMultiplyTask
     {
-        public adding()
+        public static double AddAndMultiply(double a, double b, double c)
         {
+            return (a + b) * c;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(AddAndMultiply(3, 6, 35));        // 315
+            Console.WriteLine(AddAndMultiply(-12, 5, 17));      // -119
+            Console.WriteLine(AddAndMultiply(-40, 50, 60));     // 600
+            Console.WriteLine(AddAndMultiply(1.7, 9.9, 0.01));  // 0.116
         }
     }
 }
+
